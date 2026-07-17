16fd5afb2f99585ebdea5ea17efdfb63448c9a32
NickyHariniaina <ferrissushi@gmail.com>
2026-07-17 14:59:33 +0300

    feat(dim3): implement load_to_warehouse and add prepare_dimensions task

--- dags/tasks.py
+++ dags/tasks.py
@@ -1,9 +1,13 @@
+import pandas as pd
+
 from config.settings import Settings
-from src.extract.city_extractor import get_city_names
+from src.extract.city_extractor import get_city_names, load_cities
 from src.extract.aqi_extractor import extract_hourly
 from src.load.csv import save_raw_hourly
+from src.load.postgres import PostgresLoader
 from src.transform.quality.dataframe_cleaner import DataFrameCleaner
-from src.transform.transformer.aqi import rebuild_clean_from_raw, transform_hourly_aqi
+from src.transform.transformer.aqi import build_fact_aqi, rebuild_clean_from_raw, transform_hourly_aqi
+from src.transform.transformer.dim_date import build_dim_date
 
 
 def validate_settings(**context):
@@ -28,5 +32,19 @@ def rebuild_clean_task(**context):
     return "rebuild_ok"
 
 
+def prepare_dimensions(**context):
+    dim_city = load_cities()
+    clean_df = pd.read_csv(Settings.HOURLY_COMBINED_PATH)
+    dim_date = build_dim_date(clean_df)
+    fact_aqi = build_fact_aqi(clean_df, dim_city, dim_date)
+    fact_aqi.to_csv(Settings.FACT_AQI_PATH, index=False)
+    return "dimensions_ok"
+
+
 def load_to_warehouse(**context):
+    dim_city = pd.read_csv(Settings.CITY_CSV_PATH)
+    dim_date = pd.read_csv(Settings.DATE_CSV_PATH)
+    fact_aqi = pd.read_csv(Settings.FACT_AQI_PATH)
+    loader = PostgresLoader(Settings.get_database_url())
+    loader.save_star_schema(dim_city, dim_date, fact_aqi, Settings.POSTGRES_SCHEMA)
     return "load_ok"
